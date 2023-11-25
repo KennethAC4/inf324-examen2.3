8ac8e8dd076a20f2e6b92ace8944b560b7ab4617
KennethAC4 <147218634+KennethAC4@users.noreply.github.com>
2023-11-25 11:57:14 -0400

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Limpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(131, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(662, 552);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cargar imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "leer pos [10,10]";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Guardar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "c negro texturas";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 216);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 244);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 282);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 310);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 338);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(802, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(510, 587);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Seleccionar);
+            // 
+            // Limpiar
+            // 
+            this.Limpiar.Location = new System.Drawing.Point(3, 396);
+            this.Limpiar.Name = "Limpiar";
+            this.Limpiar.Size = new System.Drawing.Size(122, 23);
+            this.Limpiar.TabIndex = 15;
+            this.Limpiar.Text = "Limpiar";
+            this.Limpiar.UseVisualStyleBackColor = true;
+            this.Limpiar.Click += new System.EventHandler(this.Limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1324, 576);
+            this.Controls.Add(this.Limpiar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Limpiar;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        int Rm, Gm, Bm;
+        int Rmc, Gmc, Bmc, L = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server=(local);user=inf324;pwd=123456;database=texturas";
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = "select * from tchompas";
+            ada.SelectCommand.CommandType = CommandType.Text;
+            DataSet ds = new DataSet();
+            ada.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            //dataGridView1.DataBind();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.Filter = "Archivos JPG|*.JPG|Archivos BMP|*.bmp";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != string.Empty)
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            c = bmp.GetPixel(10, 10);
+            textBox1.Text = c.R.ToString();
+            textBox2.Text = c.G.ToString();
+            textBox3.Text = c.B.ToString();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = new Color();
+            c = bmp.GetPixel(e.X, e.Y);
+            Rm = c.R;
+            Gm = c.G;
+            Bm = c.B;
+            Rmc = 0; Gmc = 0; Bmc = 0;
+
+            if (textBox1.Text.Length == 0)
+            {
+                textBox1.Text = Rm.ToString();
+                textBox2.Text = Gm.ToString();
+                textBox3.Text = Bm.ToString();
+            }
+            else if (textBox4.Text.Length == 0)
+            {
+                textBox4.Text = Rm.ToString();
+                textBox5.Text = Gm.ToString();
+                textBox6.Text = Bm.ToString();
+            }
+
+            else if (textBox7.Text.Length == 0)
+            {            
+                textBox7.Text = Rm.ToString();
+                textBox8.Text = Gm.ToString();
+                textBox9.Text = Bm.ToString();
+            }
+            //if (textBox1.Text.Length == 0)
+            //{
+            //    for (int i = e.X - 5; i < e.X + 5; i++)
+            //        for (int j = e.Y - 5; j < e.Y + 5; j++)
+            //        {
+            //            c = bmp.GetPixel(i, j);
+            //            Rmc = Rmc + c.R; Gmc = Gmc + c.G; Bmc = Bmc + c.B;
+            //        }
+            //    Rmc = (int)Rmc / (L * L);
+            //    Gmc = (int)Gmc / (L * L);
+            //    Bmc = (int)Bmc / (L * L);
+
+            //    textBox1.Text = Rmc.ToString();
+            //    textBox2.Text = Gmc.ToString();
+            //    textBox3.Text = Bmc.ToString();
+            //}
+            //else if (textBox4.Text.Length == 0)
+            //{
+            //    for (int i = e.X - 5; i < e.X + 5; i++)
+            //        for (int j = e.Y - 5; j < e.Y + 5; j++)
+            //        {
+            //            c = bmp.GetPixel(i, j);
+            //            Rmc = Rmc + c.R; Gmc = Gmc + c.G; Bmc = Bmc + c.B;
+            //        }
+            //    Rmc = (int)Rmc / (L * L);
+            //    Gmc = (int)Gmc / (L * L);
+            //    Bmc = (int)Bmc / (L * L);
+
+            //    textBox4.Text = Rmc.ToString();
+            //    textBox5.Text = Gmc.ToString();
+            //    textBox6.Text = Bmc.ToString();
+            //}
+
+            //else if (textBox7.Text.Length == 0)
+            //{
+            //    for (int i = e.X - 5; i < e.X + 5; i++)
+            //        for (int j = e.Y - 5; j < e.Y + 5; j++)
+            //        {
+            //            c = bmp.GetPixel(i, j);
+            //            Rmc = Rmc + c.R; Gmc = Gmc + c.G; Bmc = Bmc + c.B;
+            //        }
+            //    Rmc = (int)Rmc / (L * L);
+            //    Gmc = (int)Gmc / (L * L);
+            //    Bmc = (int)Bmc / (L * L);
+
+            //    textBox7.Text = Rmc.ToString();
+            //    textBox8.Text = Gmc.ToString();
+            //    textBox9.Text = Bmc.ToString();
+            //}
+
+            //for(int i=e.X-5;i<e.X+5;i++)
+            //    for (int j = e.Y - 5; j < e.Y + 5; j++)
+            //    {
+            //        c = bmp.GetPixel(i, j);
+            //        Rmc = Rmc + c.R; Gmc = Gmc + c.G; Bmc = Bmc + c.B;
+            //    }
+            //Rmc = (int)Rmc / (L * L);
+            //Gmc = (int)Gmc / (L * L);
+            //Bmc = (int)Bmc / (L * L);
+
+            //textBox1.Text = textBox1.Text + " " + Rmc.ToString();
+            //textBox2.Text = textBox2.Text + " " + Gmc.ToString();
+            //textBox3.Text = textBox3.Text + " " + Bmc.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "server=(local);user=inf324;pwd=123456;database=texturas";
+            SqlCommand cmd = new SqlCommand();
+            string sql = string.Empty;
+            sql = sql + "insert into tchompas(ro1,gr1,bl1,ro2,gr2,bl2,ro3,gr3,bl3) values ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "','" + textBox7.Text + "','" + textBox8.Text + "','" + textBox9.Text + "')";
+            cmd.CommandText = sql;
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            SqlDataAdapter ada = new SqlDataAdapter();
+            ada.SelectCommand = new SqlCommand();
+            ada.SelectCommand.Connection = con;
+            ada.SelectCommand.CommandText = "select * from tchompas";
+            ada.SelectCommand.CommandType = CommandType.Text;
+            DataSet ds = new DataSet();
+            ada.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+            int ro1 = Convert.ToInt32(textBox1.Text);
+            int gr1 = Convert.ToInt32(textBox2.Text);
+            int bl1 = Convert.ToInt32(textBox3.Text);
+
+            int ro2 = Convert.ToInt32(textBox4.Text);
+            int gr2 = Convert.ToInt32(textBox5.Text);
+            int bl2 = Convert.ToInt32(textBox6.Text);
+
+            int ro3 = Convert.ToInt32(textBox7.Text);
+            int gr3 = Convert.ToInt32(textBox8.Text);
+            int bl3 = Convert.ToInt32(textBox9.Text);   
+
+            int mR = 0, mG = 0, mB = 0;
+            Color c = new Color();
+            Bitmap bmpR = new Bitmap(bmp.Width, bmp.Height);
+            for (int i = 0; i < bmp.Width - 10; i = i + 10)
+                for (int j = 0; j < bmp.Height - 10; j = j + 10)
+                {
+                    mR = 0;
+                    mG = 0;
+                    mB = 0;
+
+                    for (int ki = i; ki < i + 10; ki++)
+                        for (int kj = j; kj < j + 10; kj++)
+                        {
+                            c = bmp.GetPixel(ki, kj);
+                            mR = mR + c.R;
+                            mG = mG + c.G;
+                            mB = mB + c.B;
+                        }
+                    mR = mR / 100;
+                    mG = mG / 100;
+                    mB = mB / 100;
+
+                    c = bmp.GetPixel(i, j);
+                    if (((ro1 - 20 < mR) && (mR < ro1 + 20)) && ((gr1 - 20 < mG) && (mG < gr1 + 20)) && ((bl1 - 20 < mB) && (mB < bl1 + 20)) || (((ro2 - 20 < mR) && (mR < ro2 + 20)) && ((gr2 - 20 < mG) && (mG < gr2 + 20)) && ((bl2 - 20 < mB) && (mB < bl2 + 20))) || (((ro3 - 20 < mR) && (mR < ro3 + 20)) && ((gr3 - 20 < mG) && (mG < gr3 + 20)) && ((bl3 - 20 < mB) && (mB < bl3 + 20))))
+                    {
+                        for (int ki = i; ki < i + 10; ki++)
+                            for (int kj = j; kj < j + 10; kj++)
+                                bmpR.SetPixel(ki, kj, Color.Fuchsia);
+                    }
+                    else
+                    {
+                        for (int ki = i; ki < i + 10; ki++)
+                            for (int kj = j; kj < j + 10; kj++)
+                            {
+                                c = bmp.GetPixel(ki, kj);
+                                bmpR.SetPixel(ki, kj, Color.FromArgb(c.R, c.G, c.B));
+                            }
+
+                    }
+                }
+            pictureBox1.Image = bmpR;    
+        }
+
+        private void Seleccionar(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int indice = e.RowIndex;
+
+            textBox1.Text = dataGridView1.Rows[indice].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[indice].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[indice].Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[indice].Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[indice].Cells[5].Value.ToString();
+            textBox6.Text = dataGridView1.Rows[indice].Cells[6].Value.ToString();
+            textBox7.Text = dataGridView1.Rows[indice].Cells[7].Value.ToString();
+            textBox8.Text = dataGridView1.Rows[indice].Cells[8].Value.ToString();
+            textBox9.Text = dataGridView1.Rows[indice].Cells[9].Value.ToString();
+        }
+
+        private void Limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+        }
+    }
+}
